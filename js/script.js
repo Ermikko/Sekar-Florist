884f7acdb289a95a7eaa5f299dce38ab41cceafb
Ermikko <123264724+Ermikko@users.noreply.github.com>
2024-07-03 23:29:18 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Toogle class active
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//Toogle hide
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
